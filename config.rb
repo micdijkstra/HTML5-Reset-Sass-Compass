6064a3c267523657d9ca65da6d1b07ce5286e7f2
Michael Dijkstra <michael@ninety9creative.com.au>
2011-06-16 12:49:31 +1000

    Initial commit with Sass

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "_/css"
+sass_dir = "_/sass"
+images_dir = "_/images"
+javascripts_dir = "_/javascripts"
+output_style = :compact # choose from :nested, :expanded, :compact, :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
